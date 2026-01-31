f0afbbb036cf7957b289ea54407603411d0c9c79
Dilnaz <niiazdi2406_aehit@students.vizja.pl>
2026-01-31 13:24:08 +0600

    Delete src/main/java/com/codeonlineshop/onlineshop directory

--- src/main/java/com/codeonlineshop/onlineshop/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.codeonlineshop.onlineshop;
-
-public interface PaymentService {
-    void processPayment(double amount);
-}
--- src/main/java/com/codeonlineshop/onlineshop/StripePaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.codeonlineshop.onlineshop;
-
-public class StripePaymentService implements PaymentService {
-    @Override
-    public void processPayment(double amount){
-        System.out.println("Stripe Payment Service");
-        System.out.println("Amount: " + amount);
-    }
-}
